56e59fb388040984ca2d839b155df82c85b07dd5
avderevo <freeyura78@gmail.com>
2019-01-23 18:08:20 +0300

    поправил конфиги

--- Deploy/start_in_docker.sh
+++ Deploy/start_in_docker.sh
@@ -6,11 +6,11 @@ set -u
 export DJANGO_SETTINGS_MODULE=ITS_api.settings
 export DJANGO_CONFIGURATION=Dev
 
-cd /opt/app
+#cd /opt/app
 python3 manage.py migrate --noinput        # Apply database migrations
 #python3 manage.py collectstatic --noinput  # Collect static files
 python3 manage.py createdefaultuser   # Create default user admin
 python3 manage.py loaddata course.json lessons.json
 python3 manage.py runserver
 
-supervisord -c /opt/app/Deploy/supervisor.conf
+#supervisord -c /opt/app/Deploy/supervisor.conf
